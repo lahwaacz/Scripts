2fa2e36c22c7424e869901ba1caacd7b6ab31e75
Jakub Klinkovský <j.l.k@gmx.com>
2013-05-11 19:34:07 +0200

    some more scripts

--- pdf-extract.sh
+++ pdf-extract.sh
@@ -4,7 +4,7 @@ any2img() {
     convert -density 150 "$1" -quality 100 "$2" &>/dev/null
 }
 
-pdf2jpg() {
+pdf2img() {
     echo "Splitting single pdf file by pages and converting to png"
     stapler burst "$1"
     rm -f "$1"
@@ -17,7 +17,7 @@ pdf2jpg() {
     done
 }
 
-djvu2jpg() {
+djvu2img() {
     echo "Splitting single djvu file by pages and converting to jpg"
     pages=`djvused -e "n" "$1"`
     for (( i=1; i<=$pages; i++ ))
@@ -37,9 +37,9 @@ if [[ $1 == *.pdf ]]; then
     mkdir "$dir"
     cp "$1" "$dir"
     cd "$dir"
-    pdf2jpg "$1"
+    pdf2img "$1"
 elif [[ $1 == *.djvu ]]; then
-    djvu2jpg "$1"
+    djvu2img "$1"
 else
     echo "Supported file types: pdf, djvu"
     exit 1
