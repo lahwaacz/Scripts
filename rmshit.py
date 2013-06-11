0a84ba8bbb1f2d5631fbd2f5866e0de11b85ccb6
Jakub Klinkovský <j.l.k@gmx.com>
2013-06-11 15:20:39 +0200

    added rmshit.py

---
+++ rmshit.py
@@ -0,0 +1,69 @@
+#! /usr/bin/env python
+
+import os
+import sys
+import shutil
+
+
+shittyfiles = [
+    '~/.adobe',              # Flash crap
+    '~/.macromedia',         # Flash crap
+    '~/.recently-used',
+    '~/.local/share/recently-used.xbel',
+    '~/Desktop',             # Firefox creates this
+    '~/.bzr.log',
+    '~/.thumbnails',
+    '~/.gconfd',
+    '~/.gconf',
+    '~/.local/share/gegl-0.2',
+    '~/.FRD/log/app.log',
+    '~/.FRD/links.txt',
+    '~/.objectdb',
+    '~/.gstreamer-0.10',
+    '~/.pulse',
+    '~/.esd_auth',
+]
+
+
+def yesno(question, default="n"):
+    """ Asks the user for YES or NO, always case insensitive.
+        Returns True for YES and False for NO.
+    """
+    prompt = "%s (y/[n]) " % question
+
+    ans = input(prompt).strip().lower()
+
+    if not ans:
+        ans = default
+
+    if ans == "y":
+        return True
+    return False
+
+
+def rmshit():
+    print("Found shittyfiles:")
+    found = []
+    for f in shittyfiles:
+        absf = os.path.expanduser(f)
+        if os.path.exists(absf):
+            found.append(absf)
+            print("    %s" % f)
+
+    if len(found) == 0:
+        print("No shitty files found :)")
+        return
+
+    if yesno("Remove all?", default="n"):
+        for f in found:
+            if os.path.isfile(f):
+                os.remove(f)
+            else:
+                shutil.rmtree(f)
+        print("All cleaned")
+    else:
+        print("No file removed")
+
+
+if __name__ == '__main__':
+    rmshit()
