bb20a2d445c14e509ef7452d0599a84b3f841e13
Jakub Klinkovský <j.l.k@gmx.com>
2015-08-21 21:38:36 +0200

    update count-unread-mails.sh

--- count-unread-mails.sh
+++ count-unread-mails.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 # count new emails of all accounts (for display in statusbar)
+find ~/Maildir/FJFI/INBOX/new/ -type f | wc -l > /dev/shm/new-mails-count
 #find ~/Maildir/*/*/new/ -type f | wc -l > /dev/shm/new-mails-count
 #find ~/Maildir/*/INBOX/new/ -type f | wc -l > /dev/shm/new-mails-count
-find ~/Maildir/*/{INBOX,arch,arch-general,arch-wiki}/new/ -type f | wc -l > /dev/shm/new-mails-count
+#find ~/Maildir/*/{INBOX,arch,arch-general,arch-wiki}/new/ -type f | wc -l > /dev/shm/new-mails-count
