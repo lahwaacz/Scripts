d68360ff4e4b6e958a29715b8c24495c3af5fc0e
Jakub Klinkovský <j.l.k@gmx.com>
2015-08-21 21:40:49 +0200

    update backup-system.sh

--- backup-system.sh
+++ backup-system.sh
@@ -12,18 +12,18 @@ rootdir="$1/backup-root"
 
 START=$(date +%s)
 
-echo "Creating snapshots of some subvolumes ('bsnap.sh snapshot')"
+echo "Syncing / to $rootdir (root permissions required)"
+sudo rsyncbtrfs backup / "$rootdir" -aPhAHX --info=progress2 --stats --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found","/home","/swapfile"}
+
+echo "Syncing ~/ to $homedir"
+# --one-file-system skips subvolumes
+sudo rsyncbtrfs backup ~/ $homedir -aPhAHX --one-file-system --info=progress2 --stats --exclude={"/Filmy-NEW","/Filmy-OLD","/.cache/ccache/","/.thumbnails/","/_backup_snapshots/","/rsnapshots/","/aur/"}
+
+#echo "Creating snapshots of some subvolumes ('bsnap.sh snapshot')"
 #bsnap.sh snapshot
 
 echo "Transferring snapshots ('bsnap.sh transfer $snapdir')"
 bsnap.sh transfer "$snapdir"
-
-echo "Syncing ~/ to $homedir"
-# --one-file-system skips subvolumes
-sudo rsyncbtrfs backup ~/ $homedir -aPhAHX --one-file-system --stats --exclude={"/Filmy-NEW","/Filmy-OLD","/.cache/ccache/","/.thumbnails/","/_backup_snapshots/","/rsnapshots/","/aur/"}
-
-echo "Syncing / to $rootdir (root permissions required)"
-sudo rsyncbtrfs backup / "$rootdir" -aPhAHX --stats --exclude={/dev/*,/proc/*,/sys/*,/tmp/*,/run/*,/mnt/*,/media/*,/lost+found,/home,/swapfile}
 
 FINISH=$(date +%s)
 echo "total time: $(( ($FINISH-$START) / 60 )) minutes, $(( ($FINISH-$START) % 60 )) seconds"
