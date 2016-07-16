0d4b739e4ca393937b0dc388f0be6a9ffaf7646a
Jakub Klinkovský <j.l.k@gmx.com>
2016-07-16 12:11:22 +0200

    update backup-system.sh

--- backup-system.sh
+++ backup-system.sh
@@ -3,27 +3,28 @@
 # exit on first error
 set -e
 
-# check if destination dir exists
-[[ ! -d $1 ]] && echo -e "Set valid destination folder as \$1.\nExiting..." && exit 1
+backupdir="/media/black/backups"
+music="/media/black/Music"
 
-snapdir="$1/backup-lahwaacz"
-homedir="$snapdir/home"
-rootdir="$1/backup-root"
+# check if destination dir exists
+if [[ ! -d "$backupdir" ]]; then
+    echo "Backup directory $backupdir does not exist. Is the drive mounted?"
+    exit 1
+fi
+
+homedir="$backupdir/home_lahwaacz"
+rootdir="$backupdir/root"
 
 START=$(date +%s)
 
 echo "Syncing / to $rootdir (root permissions required)"
-sudo rsyncbtrfs backup / "$rootdir" -aPhAHX --info=progress2 --stats --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found","/home","/swapfile"}
+sudo rsync / "$rootdir" -aPhAHX --info=progress2,name0,stats2 --delete --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found","/home","/swapfile"}
+
+echo "Syncing ~/Music/ to $music"
+rsync ~/Music/ $music -aPhAHX --info=progress2,name0,stats2
 
 echo "Syncing ~/ to $homedir"
-# --one-file-system skips subvolumes
-sudo rsyncbtrfs backup ~/ $homedir -aPhAHX --one-file-system --info=progress2 --stats --exclude={"/Filmy-NEW","/Filmy-OLD","/.cache/ccache/","/.thumbnails/","/_backup_snapshots/","/rsnapshots/","/aur/"}
-
-#echo "Creating snapshots of some subvolumes ('bsnap.sh snapshot')"
-#bsnap.sh snapshot
-
-echo "Transferring snapshots ('bsnap.sh transfer $snapdir')"
-bsnap.sh transfer "$snapdir"
+rsync ~/ $homedir -aPhAHX --one-file-system --info=progress2,name0,stats2 --delete --exclude={"/build/builddir/","/build/pkgs/","/build/src/","/mnt/","/stuff/"}
 
 FINISH=$(date +%s)
 echo "total time: $(( ($FINISH-$START) / 60 )) minutes, $(( ($FINISH-$START) % 60 )) seconds"
