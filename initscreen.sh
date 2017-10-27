d2be8a7d3aeb1c15f6a2ed00d34657b982c34e3c
Jakub Klinkovský <j.l.k@gmx.com>
2017-10-27 12:53:25 +0200

    generalize initscreen.sh

--- initscreen.sh
+++ initscreen.sh
@@ -9,22 +9,27 @@ connected=$(xrandr | grep " connected" | sed -e "s/\([A-Z0-9]\+\) connected.*/\1
 
 #echo "initscreen.sh: hdmi $hdmi; vga $vga"
 
-if [[ $connected =~ "HDMI-0" ]]; then
-    # hdmi only
-    # NOTE: i3 fails if no active output is detected, so we have to first enable second output and then disable the first
-#    xrandr --nograb --output HDMI-0 --auto --primary
-#    xrandr --nograb --output LVDS-0 --off
-    # both
-    # HDMI-0 is primary, LVDS-0 is panned to be vertically aligned to the bottom
-#    xrandr --nograb --output HDMI-0 --auto --primary --output LVDS-0 --auto --left-of HDMI-0 --panning 1366x768+0+312
-#    xrandr --output HDMI-0 --auto --primary --output LVDS-0 --auto --left-of HDMI-0 --panning 1366x768+0+312
-    xrandr --output HDMI-0 --auto --primary --output LVDS-0 --auto --right-of HDMI-0
-elif [[ $connected =~ "VGA-0" ]]; then
-#    xrandr --nograb --output VGA-0 --auto --output LVDS-0 --mode 1024x768 --primary
-    # TODO:  look at --scale argument
-    xrandr --output VGA-0 --auto --primary --output LVDS-0 --auto --below VGA-0
+if [[ $connected =~ "LVDS-0" ]]; then
+    if [[ $connected =~ "HDMI-0" ]]; then
+        # hdmi only
+        # NOTE: i3 fails if no active output is detected, so we have to first enable second output and then disable the first
+    #    xrandr --nograb --output HDMI-0 --auto --primary
+    #    xrandr --nograb --output LVDS-0 --off
+        # both
+        # HDMI-0 is primary, LVDS-0 is panned to be vertically aligned to the bottom
+    #    xrandr --nograb --output HDMI-0 --auto --primary --output LVDS-0 --auto --left-of HDMI-0 --panning 1366x768+0+312
+    #    xrandr --output HDMI-0 --auto --primary --output LVDS-0 --auto --left-of HDMI-0 --panning 1366x768+0+312
+        xrandr --output HDMI-0 --auto --primary --output LVDS-0 --auto --right-of HDMI-0
+    elif [[ $connected =~ "VGA-0" ]]; then
+    #    xrandr --nograb --output VGA-0 --auto --output LVDS-0 --mode 1024x768 --primary
+        # TODO:  look at --scale argument
+        xrandr --output VGA-0 --auto --primary --output LVDS-0 --auto --below VGA-0
+    else
+    #    xrandr --nograb --output LVDS-0 --auto --primary --output HDMI-0 --off
+    #    xrandr --output LVDS-0 --auto --primary --output HDMI-0 --off
+        xrandr --output LVDS-0 --auto --primary --output HDMI-0 --off --output VGA-0 --off
+    fi
 else
-#    xrandr --nograb --output LVDS-0 --auto --primary --output HDMI-0 --off
-#    xrandr --output LVDS-0 --auto --primary --output HDMI-0 --off
-    xrandr --output LVDS-0 --auto --primary --output HDMI-0 --off --output VGA-0 --off
+    first=$(echo $connected | cut -f1 -d' ')
+    xrandr --output ${first} --auto --primary
 fi
