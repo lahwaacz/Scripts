dcd3746d1203e85e707d2fa7caf976a2d9ce563c
Jakub Klinkovský <j.l.k@gmx.com>
2019-04-18 20:16:08 +0200

    update initscreen.sh

--- initscreen.sh
+++ initscreen.sh
@@ -5,7 +5,11 @@ set -e
 
 #hdmi=$(cat /sys/class/drm/card0-HDMI-A-1/status)
 #vga=$(cat /sys/class/drm/card0-VGA-1/status)
-connected=$(xrandr | grep " connected" | sed -e "s/\([A-Z0-9]\+\) connected.*/\1/")
+if [[ "$WAYLAND_DISPLAY" == "" ]]; then
+    connected=$(xrandr | grep " connected" | sed -e "s/\([A-Z0-9]\+\) connected.*/\1/")
+else
+    connected=$(swaymsg -pt get_outputs | grep -E "^Output" | awk '{print $2}')
+fi
 
 #echo "initscreen.sh: hdmi $hdmi; vga $vga"
 
@@ -28,6 +32,34 @@ if [[ $connected =~ "LVDS-0" ]]; then
     #    xrandr --nograb --output LVDS-0 --auto --primary --output HDMI-0 --off
     #    xrandr --output LVDS-0 --auto --primary --output HDMI-0 --off
         xrandr --output LVDS-0 --auto --primary --output HDMI-0 --off --output VGA-0 --off
+    fi
+elif [[ $connected =~ "eDP-1" ]]; then
+    if [[ -f /proc/acpi/button/lid/LID/state ]]; then
+        lid=$(cat /proc/acpi/button/lid/LID/state | awk '{print $2}')
+    else
+        lid="open"
+    fi
+    if [[ "$WAYLAND_DISPLAY" == "" ]]; then
+        if [[ $connected =~ "HDMI-1" ]] && [[ "$lid" == "closed" ]]; then
+            xrandr --output HDMI-1 --auto --primary --output eDP-1 --off
+            echo "Xft.dpi: 96" | xrdb -merge
+        elif [[ $connected =~ "HDMI-1" ]]; then
+            xrandr --output HDMI-1 --auto --primary --output eDP-1 --auto --left-of HDMI-1
+        else
+            xrandr --output eDP-1 --auto --primary --output HDMI-1 --off
+            echo "Xft.dpi: 168" | xrdb -merge   # scale=1.75
+        fi
+    else
+        if [[ $connected =~ "HDMI-A-1" ]] && [[ "$lid" == "closed" ]]; then
+            swaymsg output HDMI-A-1 enable
+            swaymsg output eDP-1 disable
+        elif [[ $connected =~ "HDMI-A-1" ]]; then
+            swaymsg output HDMI-A-1 enable
+            swaymsg output eDP-1 enable
+        else
+            swaymsg output eDP-1 enable
+            swaymsg output HDMI-A-1 disable
+        fi
     fi
 else
     first=$(echo $connected | cut -f1 -d' ')
