451fe02221b7e6893727a74b35bc8870e8812d7b
M. Yas. Davoodeh <30480116+MYDavoodeh@users.noreply.github.com>
2020-02-23 16:55:47 +0330

    Making it more general. Works on any device now.
    Using grep -P to get the device no matter what is it as far as it is called a touchpad.
    Not depending on Awk anymore.
    
    Idk why would anybody care but I'm proud that it uses one less variable xD

--- toggle-touchpad.sh
+++ toggle-touchpad.sh
@@ -1,12 +1,11 @@
-#! /bin/bash
+#!/bin/sh
+# Toggle touchpad status
+# Using libinput and xinput
 
-# toggle touchpad status
+# Use xinput list and do a search for touchpads. Then get the first one and get its name.
+device="$(xinput list | grep -P '(?<= )[\w\s:]*(?i)touchpad(?-i).*?(?=\s*id)' -o | head -n1)"
 
-device="SynPS/2 Synaptics TouchPad"
-enabled=$(xinput --list-props "$device" | grep "Device Enabled" | awk '{print $NF}')
-
-if [[ "$enabled" == "1" ]]; then
-    xinput --disable "$device"
-else
-    xinput --enable "$device"
-fi
+# If it was activated disable it and if it wasn't disable it
+[[ "$(xinput list-props "$device" | grep -P ".*Device Enabled.*\K.(?=$)" -o)" == "1" ]] &&
+    xinput disable "$device" ||
+    xinput enable "$device"
