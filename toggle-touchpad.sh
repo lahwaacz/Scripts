fa16d6ade0d576ff2ff60ffca922e6f2dc4064a7
M. Yas. Davoodeh <30480116+MYDavoodeh@users.noreply.github.com>
2020-04-10 23:57:13 +0430

    Make it work on Synaptic touchpads too

--- toggle-touchpad.sh
+++ toggle-touchpad.sh
@@ -3,7 +3,7 @@
 # Using libinput and xinput
 
 # Use xinput list and do a search for touchpads. Then get the first one and get its name.
-device="$(xinput list | grep -P '(?<= )[\w\s:]*(?i)touchpad(?-i).*?(?=\s*id)' -o | head -n1)"
+device="$(xinput list | grep -P '(?<= )[\w\s:]*(?i)(touchpad|synaptics)(?-i).*?(?=\s*id)' -o | head -n1)"
 
 # If it was activated disable it and if it wasn't disable it
 [[ "$(xinput list-props "$device" | grep -P ".*Device Enabled.*\K.(?=$)" -o)" == "1" ]] &&
