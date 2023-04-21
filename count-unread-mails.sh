4317390d6c6aaa248d3fbaed177291b3c9d35efe
Jakub Klinkovský <j.l.k@gmx.com>
2023-04-21 21:39:51 +0200

    ditch mail counting script

--- count-unread-mails.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-# count new emails of all accounts (for display in statusbar)
-find ~/Maildir/FJFI/*/new/ -type f | wc -l > /dev/shm/new-mails-count
-#find ~/Maildir/*/*/new/ -type f | wc -l > /dev/shm/new-mails-count
-#find ~/Maildir/*/INBOX/new/ -type f | wc -l > /dev/shm/new-mails-count
-#find ~/Maildir/*/{INBOX,arch,arch-general,arch-wiki}/new/ -type f | wc -l > /dev/shm/new-mails-count
