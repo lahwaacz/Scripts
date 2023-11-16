362adb554c9fee13fc966eb88634fd568be40ba9
Jakub Klinkovský <j.l.k@gmx.com>
2023-11-16 20:40:28 +0100

    add btrfs-sync and update backup-system.sh

--- backup-system.sh
+++ backup-system.sh
@@ -3,8 +3,7 @@
 # exit on first error
 set -e
 
-backupdir="/media/black/backups"
-music="/media/black/Music"
+backupdir="/media/WD-black/backups"
 
 # check if destination dir exists
 if [[ ! -d "$backupdir" ]]; then
@@ -12,19 +11,24 @@ if [[ ! -d "$backupdir" ]]; then
     exit 1
 fi
 
-homedir="$backupdir/home_lahwaacz"
-rootdir="$backupdir/root"
+#homedir="$backupdir/home_rsync_copy"
+#rootdir="$backupdir/root_rsync_copy"
 
-START=$(date +%s)
+#echo "Syncing / to $rootdir (root permissions required)"
+#sudo rsync / "$rootdir" -aPhAHX --info=progress2,name0,stats2 --delete --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found","/home","/swapfile","/.snapshots"}
 
-echo "Syncing / to $rootdir (root permissions required)"
-sudo rsync / "$rootdir" -aPhAHX --info=progress2,name0,stats2 --delete --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found","/home","/swapfile"}
+#echo "Syncing ~/ to $homedir"
+#rsync ~/ $homedir -aPhAHX --one-file-system --info=progress2,name0,stats2 --delete
 
-echo "Syncing ~/Music/ to $music"
-rsync ~/Music/ $music -aPhAHX --delete --info=progress2,name0,stats2
 
-echo "Syncing ~/ to $homedir"
-rsync ~/ $homedir -aPhAHX --one-file-system --info=progress2,name0,stats2 --delete --exclude={"/build/builddir/","/build/pkgs/","/build/src/","/mnt/","/Music/"}
+# TODO:
+# - make snapshot with snapper just before btrfs-sync
+# - run `sync` before btrfs-sync to make sure that the snapshot is fully written to the disk
+# - copy the snapper metadata files (info.xml)
+# - make snapshots of the remaining subvolumes: @postgres @nspawn_containers @var_log
 
-FINISH=$(date +%s)
-echo "total time: $(( ($FINISH-$START) / 60 )) minutes, $(( ($FINISH-$START) % 60 )) seconds"
+echo "Syncing /.snapshots to $backupdir/root (root permissions required)"
+sudo btrfs-sync --verbose --delete /.snapshots "$backupdir/root"
+
+echo "Syncing /home/.snapshots to $backupdir/home (root permissions required)"
+sudo btrfs-sync --verbose --delete /home/.snapshots "$backupdir/home"
